113bf10bb4f51e5e69df41b0ce0f767ceb706018
Caroline <wanguic49@gmail.com>
2023-12-15 12:58:41 +0300

    few changes made

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+
+  currentVal:number = 1;
+  
+  decselected:boolean = false;
+  increselected:boolean = false;
 
   constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  increase(){
+    this.increselected = true;
+    this.decselected = false;
+    this.currentVal ++;
+
+  }
+  decrease(){
+    this.decselected = true;
+    this.increselected = false;
+    if(this.currentVal > 1){
+      this.currentVal --;
+    }
+  }
 
 }

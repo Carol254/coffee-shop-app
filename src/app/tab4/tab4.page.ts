a206aa29548182fbbc4918b725d6af136250f494
Caroline <wanguic49@gmail.com>
2023-12-20 16:32:03 +0300

    few changes made

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonModal)modal!: IonModal;
+
+  message:any;
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  cancel(){
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm(){
+    this.modal.dismiss(this.message ,'confirm');
+  }
+
+  onWillDimiss(event:Event){
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if(ev.detail.role === 'confirm'){
+      this.message = this.router.navigate(['/login']);
+      ev.detail.data = this.message;
+    }
   }
 
 }
